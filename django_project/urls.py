687f19fb2aaadbb205203f71ade6d1f642d1d9ca
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-06-29 12:49:58 +0300

    setup user

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,7 +23,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("blogs.urls")),
     path('users',user_views.register,name="register"),
-    path('login',auth_views.LoginView.as_view(), name="login"),
-    path("logout/",auth_views.LogoutView.as_view(),name='logout'),
+    path('profile',user_views.profile,name="profile"),
+    path('login',auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+
+    # path('logout',auth_views.LogoutView.as_view(template_name="users/logout.html"),name='logout'),
     # path('blogs',include("blogs.urls")),
 ]
